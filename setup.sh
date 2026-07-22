f8f735836d1cdaa150fec13b883f4d58d42a2843
DrakenGuard <152512112+DrakenGuard@users.noreply.github.com>
2026-07-22 13:42:42 +0200

    Refactor for maintainability and to address process failure

--- setup.sh
+++ setup.sh
@@ -1,40 +1,82 @@
 #/usr/bin/env bash
 
-printf "This script installs vim-plug for vim and neovim and sets the theme for both to gruvbox. Are you sure you want to continue? (y/N) "
+set -euo pipefail # exit immediately if something in the process fails
 
-read prompt
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+	echo "This script only works on linux."
+	exit 1
+fi
+
+read -p "This script installs vim-plug for vim and neovim and sets the theme for both to gruvbox. Are you sure you want to continue? (y/N) " prompt
 
 if [[ ! $prompt == "y" && ! $prompt == "Y" ]]; then
 	echo "Exiting program"
 	exit 0
 fi
 
-if [[ -f /usr/bin/vim ]]; then
-	if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
-		echo "Installing vim-plug for vim"
-		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	else
-		echo "Vim-plug for vim exists. Continuing..."
-	fi
-	curl "https://raw.githubusercontent.com/DrakenGuard/personal-setup/refs/heads/main/vim-config.vim" > ~/.vimrc
-	vim -es +PlugInstall +qall
-else
-	echo "Vim not installed. Skipping config for vim..."
-fi
+: '
+The first argument for install_vimplug must be the type of vim (e.g. vim or neovim).
+'
+install_vimplug() {
+	vimplug="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
-if [[ -f /usr/bin/nvim ]]; then
-	if [[ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]]; then
-		echo "Installing vim-plug for neovim"
-		curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	else
-		echo "Vim-plug for neovim exists. Continuing..."
-	fi
-	echo "Configuring neovim..."
-	curl "https://raw.githubusercontent.com/DrakenGuard/personal-setup/refs/heads/main/neovim-config.vim" > ~/.config/nvim/init.vim
-	nvim --headless +PlugInstall +qall
-else
-	echo "Neovim not installed. Skipping config for neovim..."
-fi
+	case $1 in
+		vim)
+			vimplug_path="$HOME/.vim/autoload/plug.vim"
+			;;
+		neovim)
+			vimplug_path="$HOME/.local/share/nvim/site/autoload/plug.vim"
+			;;
+	esac
 
+	if [[ ! -f $vimplug_path ]]; then
+		echo "Installing vim-plug for $1"
+		curl -fLo $vimplug_path --create-dirs \
+			$vimplug
+	else
+		echo "Vim-plug for $1 exists. Continuing..."
+	fi
+}
+
+: '
+The first argument for set_theme_gruvbox must be the type of vim (e.g. vim or neovim).
+'
+set_theme_gruvbox() {
+	case $1 in
+		vim)
+			config=$HOME/.vimrc
+			curl "https://raw.githubusercontent.com/DrakenGuard/personal-setup/refs/heads/main/vim-config.vim" > $config
+			vim -Nu $config -es +'PlugInstall --sync' +qall
+			;;
+		neovim)
+			mkdir -p $HOME/.config/nvim
+			config=$HOME/.config/nvim/init.vim
+			curl "https://raw.githubusercontent.com/DrakenGuard/personal-setup/refs/heads/main/neovim-config.vim" > $config
+			nvim --headless -u $config +PlugInstall +qall
+			;;
+	esac
+}
+
+main() {
+	echo "Configuring for vim"
+	if [[ -f /usr/bin/vim ]]; then
+		install_vimplug vim
+		set_theme_gruvbox vim
+	else
+		echo "Vim not installed / Vim not found in '/usr/bin/vim'. Skipping config for vim..."
+	fi
+
+	echo ""
+
+	echo "Configuring for nvim"
+	if [[ -f /usr/bin/nvim ]]; then
+		install_vimplug neovim
+		set_theme_gruvbox neovim
+	else
+		echo "Neovim not installed / Neovim not found in '/usr/bin/nvim'. Skipping config for neovim..."
+	fi
+
+	echo "Setup complete!"
+}
+
+main
